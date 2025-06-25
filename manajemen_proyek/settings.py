36ca13dcb6fdba8add2b3dd317be643d81e72eb6
michaelbriant <michaelbriant13@gmail.com>
2025-06-25 20:59:18 +0700

    Fix deployment: set DEBUG=False, set ALLOWED_HOSTS, add STATIC_ROOT

--- manajemen_proyek/settings.py
+++ manajemen_proyek/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ln=dz(d)tgb5*1j=yyippb!y^b+w1+qd=e3!xm&ftr5dr0g_9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['michaelbriant.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'proyek/static'),
 ]
